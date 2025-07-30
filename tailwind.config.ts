3bdd1fc965b9a34780a39be60491fabc44485dc3
prowider.dev <prowider.dev@gmail.com>
2025-07-30 18:46:23 +0500

    asdfasdf

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,14 @@
- /** @type {import('tailwindcss').Config} */
-export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
